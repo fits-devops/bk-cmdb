30d26616e08957defb13903cf5c27b1a2979e6a2
honglimin <honglm@fits.com.cn>
2019-09-26 15:28:52 +0800

    【初始化数据】机房模型v3

--- src/scene_server/admin_server/upgrader/v3.0.8/pkg.go
+++ src/scene_server/admin_server/upgrader/v3.0.8/pkg.go
@@ -1,15 +1,3 @@
-/*
- * Tencent is pleased to support the open source community by making 蓝鲸 available.
- * Copyright (C) 2017-2018 THL A29 Limited, a Tencent company. All rights reserved.
- * Licensed under the MIT License (the "License"); you may not use this file except
- * in compliance with the License. You may obtain a copy of the License at
- * http://opensource.org/licenses/MIT
- * Unless required by applicable law or agreed to in writing, software distributed under
- * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
- * either express or implied. See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package v3v0v8
 
 import (
@@ -24,26 +12,32 @@ func init() {
 }
 
 func upgrade(ctx context.Context, db dal.RDB, conf *upgrader.Config) (err error) {
+	//创建表
 	err = createTable(ctx, db, conf)
 	if err != nil {
 		return err
 	}
+	//增加内置对象数据
 	err = addPresetObjects(ctx, db, conf)
 	if err != nil {
 		return err
 	}
+	//增加云区域数据
 	err = addPlatData(ctx, db, conf)
 	if err != nil {
 		return err
 	}
+	//增加系统数据
 	err = addSystemData(ctx, db, conf)
 	if err != nil {
 		return err
 	}
+	//增加默认的应用数据
 	err = addDefaultBiz(ctx, db, conf)
 	if err != nil {
 		return err
 	}
+	//增加后台数据
 	err = addBKApp(ctx, db, conf)
 	if err != nil {
 		return err
--- src/scene_server/admin_server/upgrader/x18.12.13.02/pkg.go
+++ src/scene_server/admin_server/upgrader/x18.12.13.02/pkg.go
@@ -1,16 +1,4 @@
-/*
- * Tencent is pleased to support the open source community by making 蓝鲸 available.
- * Copyright (C) 2017-2018 THL A29 Limited, a Tencent company. All rights reserved.
- * Licensed under the MIT License (the "License"); you may not use this file except
- * in compliance with the License. You may obtain a copy of the License at
- * http://opensource.org/licenses/MIT
- * Unless required by applicable law or agreed to in writing, software distributed under
- * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
- * either express or implied. See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-package x18_11_07_01
+package x18_12_13_02
 
 import (
 	"context"
@@ -21,32 +9,32 @@ import (
 )
 
 func init() {
-	upgrader.RegistUpgrader("x18_11_07_01", upgrade)
+	upgrader.RegistUpgrader("x18_12_13_02", upgrade)
 }
 
 func upgrade(ctx context.Context, db dal.RDB, conf *upgrader.Config) (err error) {
 
 	err = addCloudTaskTable(ctx, db, conf)
 	if err != nil {
-		blog.Errorf("[upgrade x18_11_07_01] addCloudTaskTable error  %s", err.Error())
+		blog.Errorf("[upgrade x18_12_13_02] addCloudTaskTable error  %s", err.Error())
 		return err
 	}
 
 	err = addCloudResourceConfirmTable(ctx, db, conf)
 	if err != nil {
-		blog.Errorf("[upgrade x18_11_07_01] addCloudResourceConfirmTable error  %s", err.Error())
+		blog.Errorf("[upgrade x18_12_13_02] addCloudResourceConfirmTable error  %s", err.Error())
 		return err
 	}
 
 	err = addCloudSyncHistoryTable(ctx, db, conf)
 	if err != nil {
-		blog.Errorf("[upgrade x18_11_07_01] addCloudSyncHistoryTable error  %s", err.Error())
+		blog.Errorf("[upgrade x18_12_13_02] addCloudSyncHistoryTable error  %s", err.Error())
 		return err
 	}
 
 	err = addCloudConfirmHistoryTable(ctx, db, conf)
 	if err != nil {
-		blog.Errorf("[upgrade x18_11_07_01] addCloudConfirmHistoryTable error  %s", err.Error())
+		blog.Errorf("[upgrade x18_12_13_02] addCloudConfirmHistoryTable error  %s", err.Error())
 		return err
 	}
 
